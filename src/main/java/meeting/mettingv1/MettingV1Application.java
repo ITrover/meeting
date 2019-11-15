2bbb9a55774d508d8e39569acb80a63cb52f1304
YOhonour <1083775096@qq.com>
2019-11-16 01:12:29 +0800

    初始化项目，并配置了一些基本配置比如数据库、Druid

---
+++ src/main/java/meeting/mettingv1/MettingV1Application.java
@@ -0,0 +1,13 @@
+package meeting.mettingv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MettingV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MettingV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/meeting/mettingv1/MettingV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package meeting.mettingv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MettingV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
