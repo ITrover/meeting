ce5541a271bb41867df27c8c54d3c12e34054861
YOhonour <1083775096@qq.com>
2019-11-20 17:14:23 +0800

    数据返回实体类

---
+++ src/main/java/meeting/meetingv1/util/ResultBean.java
@@ -0,0 +1,60 @@
+package meeting.meetingv1.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ResultBean {
+    private int code;
+    private String message;
+    private Map data;
+
+    private ResultBean() {
+
+    }
+
+    public static ResultBean error(int code, String message) {
+        ResultBean resultBean = new ResultBean();
+        resultBean.setCode(code);
+        resultBean.setMessage(message);
+        return resultBean;
+    }
+
+    public static ResultBean success() {
+        ResultBean resultBean = new ResultBean();
+        resultBean.setCode(0);
+        resultBean.setMessage("success");
+        return resultBean;
+    }
+
+    public static  ResultBean success(Map data) {
+        ResultBean resultBean = new ResultBean();
+        resultBean.setCode(0);
+        resultBean.setMessage("success");
+        resultBean.setData(data);
+        return resultBean;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map getData() {
+        return data;
+    }
+
+    public void setData(Map data) {
+        this.data = data;
+    }
+}
