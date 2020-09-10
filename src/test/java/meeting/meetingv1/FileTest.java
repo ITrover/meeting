54fe24982c8755085893d4d95aa739fff9bd2ed6
itrover <1172610139@qq.com>
2020-09-10 14:19:27 +0800

    修改配置文件

---
+++ src/test/java/meeting/meetingv1/FileTest.java
@@ -0,0 +1,28 @@
+package meeting.meetingv1;
+
+import javafx.application.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+
+/**
+ * @author MaYunHao
+ * @version 1.0
+ * @date 2020/9/5 18:23
+ * @description
+ */
+@SpringBootTest
+public class FileTest {
+    @Autowired
+    HttpServletRequest httpServletRequest;
+    @Value("${user.avatar.path}")
+    String avater;
+    @Test
+    void filePathTest(){
+    }
+}
