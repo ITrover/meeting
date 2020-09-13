959310c71cce8fa186bc4a38021386945daeee1b
mengxiangying <1984997880@qq.com>
2020-09-13 22:29:35 +0800

    4-6接口

--- src/main/java/meeting/meetingv1/controller/IconController.java
+++ src/main/java/meeting/meetingv1/controller/IconController.java
@@ -2,6 +2,7 @@ package meeting.meetingv1.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import meeting.meetingv1.annotation.UserLoginToken;
 import meeting.meetingv1.pojo.UserMeeting;
 import meeting.meetingv1.service.UserMeetingService;
@@ -26,9 +27,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
+@Slf4j
 @Api(tags = {"会议主题图操作接口"})
 public class IconController {
 
@@ -39,11 +43,12 @@ public class IconController {
     @Autowired
     UserMeetingService userMeetingService;
     Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @ApiOperation(value = "获取会议主题图",notes = "参数： <br>1、会议ID meetingId<br>注意：<br>   1、Resful风格接口，会议id为路径变量，比如会议1的路径为：.../meetingIcon/1 <br>   2、未上传过会议主题图返回默认图")
-    @GetMapping(value = "/meetingIcon/{meetingId}",produces = MediaType.IMAGE_PNG_VALUE)
-    public void getMeetIcon(@PathVariable String meetingId, HttpServletResponse response) throws IOException {
-        OutputStream outputStream = response.getOutputStream();
+    @ResponseBody
+    @ApiOperation(value = "获取会议主题图",notes = "参数： <br>1、会议ID meetingId<br>注意：<br>   " +
+            "1、Resful风格接口，会议id为路径变量，比如会议1的路径为：.../meetingIcon/1 <br>   2、未上传过会议主题图返回默认图")
+    @GetMapping(value = "/meetingIcon/{meetingId}",produces = "applications/json;charset=UTF-8")
+    public ResultBean getMeetingIcon(@PathVariable String meetingId, HttpServletResponse response) throws IOException {
+        //OutputStream outputStream = response.getOutputStream();
         File file = new File(meetingFileRootPath + "/" + meetingId + "/" + "icon.png");
         if (!file.exists()){
             file = new File(defaultIconPath);
@@ -55,9 +60,17 @@ public class IconController {
             response.getOutputStream().write(bytes);
         }catch (Exception e){
             stream.close();
-            return;
         }
+
         stream.close();
+
+        Map<String,Object> map=new HashMap<>();
+        String url = meetingFileRootPath + "/" + 54 + "/" + "icon.png";
+        map.put("url",url);
+        logger.error("map  : "+map.toString());
+        return ResultBean.success(map);
+
+
     }
 
     @PostMapping("/meetingIcon/{meetingId}")
@@ -77,6 +90,9 @@ public class IconController {
         File file2 = new File(meetingFileRootPath + "/" + meetingId + "/" + "icon.png");
         uploadFile.transferTo(file2);
         logger.info("会议图片上传成功！会议id:"+meetingId+" 存储路径："+fileRealPath+ "icon.png");
-        return ResultBean.success();
+        Map<String,Object> map=new HashMap<>();
+        String url = meetingFileRootPath + "/" + meetingId + "/" + "icon.png";
+        map.put("url",url);
+        return ResultBean.success(map);
     }
 }
